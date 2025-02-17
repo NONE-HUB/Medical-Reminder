9d9dd7326b56c93f67ff74989e57b8c25a694684
Swastika-9 <swasty.009@gmail.com>
2025-02-17 20:22:05 +0545

    Project added

---
+++ login.py
@@ -0,0 +1,10 @@
+from tkinter import*
+root =Tk()
+
+old_username = StringVar()
+old_password = StringVar()
+
+username1 = old_username.get()
+password1 = old_password.get()
+
+Label(root , text = "Create a username:").pack(pady=5)
